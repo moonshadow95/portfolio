564d6e8d1becbb34f1100a05c6ad882e40384da0
Moonshadow <last_enemy@naver.com>
2021-02-26 14:51:29 +0900

    JS: Handle scrolling

--- main.js
+++ main.js
@@ -11,3 +11,25 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--bg');
   }
 });
+
+// Handle scrolling when click navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+// Handle scrolling home button
+const homeBtn = document.querySelector('.home__btn');
+homeBtn.addEventListener('click', () => {
+  scrollIntoView('#about');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
