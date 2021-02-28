aba71eaca8ebda553c6dc9594654d7523a0771b1
Moonshadow <last_enemy@naver.com>
2021-02-28 13:56:57 +0900

    JS: Transparent thing

--- main.js
+++ main.js
@@ -29,6 +29,47 @@ homeBtn.addEventListener('click', () => {
   scrollIntoView('#about');
 });
 
+// Make sections fade out as window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Make about fade out as window scrolls down
+const about = document.querySelector('.about__container');
+const aboutHeght = about.getBoundingClientRect().height;
+const aboutTop = window.pageYOffset + about.getBoundingClientRect().top;
+document.addEventListener('scroll', () => {
+  about.style.opacity = 1 - (window.scrollY - aboutTop) / aboutHeght;
+});
+
+// Make skills fade out as window scrolls down
+const skills = document.querySelector('.skills__container');
+const skillsHeght = skills.getBoundingClientRect().height;
+const skillsTop = window.pageYOffset + skills.getBoundingClientRect().top;
+document.addEventListener('scroll', () => {
+  skills.style.opacity = 1 - (window.scrollY - skillsTop) / skillsHeght;
+});
+
+// Make works fade out as window scrolls down
+const works = document.querySelector('.works__container');
+const worksHeght = works.getBoundingClientRect().height;
+const worksTop = window.pageYOffset + works.getBoundingClientRect().top;
+document.addEventListener('scroll', () => {
+  works.style.opacity = 1 - (window.scrollY - worksTop) / worksHeght;
+});
+
+// Show Up Button
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add('visible');
+  } else {
+    arrowUp.classList.remove('visible');
+  }
+});
+
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({ behavior: 'smooth' });
